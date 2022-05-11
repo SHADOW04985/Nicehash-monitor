53cd3f65c6bec74914937d43104050074bd5dfbd
Aaryaman Maity <aaryamanmaity@gmail.com>
2022-05-12 01:19:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,3 @@ import os
 import sys
 if __name__ == "__main__":
     guimain.nicehashapp.gui()
-
-    def resource_path(relative_path):
-        # Get absolute path to resource, works for dev and for PyInstaller
-        try:
-            # PyInstaller creates a temp folder and stores path in _MEIPASS
-            base_path = sys._MEIPASS
-        except Exception:
-            base_path = os.path.abspath(".")
-        return os.path.join(base_path, relative_path)
